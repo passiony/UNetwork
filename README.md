0a8f36180669f478be5653aafbdf000086195746
yupu <passionyxt@gmail.com>
2024-07-15 19:31:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## UNetwork
+
+一个网络通用框架
+
+内部包括TCP，KCP，WebSocket
+
+适用于Socket客户端和Socket服务器
---
+++ Runtime/TestClient.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Text;
+using UNetwork;
+using UnityEngine;
+
+public class TestClient : MonoBehaviour
+{
+    public string ip = "127.0.0.1";
+    public int port = 12346;
+
+//    private string address = "10.200.10.192:3655";
+    public string sendMessage = "client";
+    public static long starttime = 0;
+
+    void Start()
+    {
+        ClientManager client = ClientManager.Instance;
+        client.InitService(NetworkProtocol.TCP);
+        client.MessagePacker = new ProtobufPacker();
+        client.MessageDispatcher = new OuterMessageDispatcher();
+
+        client.Connect(ip,port);
+        client.OnConnect += OnConnect;
+        client.OnError += OnError;
+        client.OnMessage += OnMessage;
+    }
+
+    private void OnMessage(byte[] obj)
+    {
+        var msg = Encoding.UTF8.GetString(obj);
+        Debug.Log($"Receive{obj.Length}" + msg);
+    }
+
+    private void OnError(int e)
+    {
+        Debug.LogError("网络错误：" + e);
+    }
+
+    private void OnConnect(int c)
+    {
+        Debug.Log("连接成功");
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            var data = Encoding.UTF8.GetBytes(sendMessage);
+            Debug.Log($"Send{data.Length}:" + sendMessage);
+
+            ClientManager.Instance.Send(data);
+            starttime = GetTimeStamp();
+        }
+    }
+
+    /// <summary>
+    /// 获取时间戳
+    /// </summary>
+    /// <returns></returns>
+    public static long GetTimeStamp()
+    {
+        return new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds();
+    }
+
+    public static void Receive()
+    {
+        var inteval = GetTimeStamp() - starttime;
+        Debug.LogWarning(inteval);
+    }
+}
