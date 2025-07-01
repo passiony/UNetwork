22fe12716ec12a590cfa6f77cfb56605b46cc7ff
Passion <563129582@qq.com>
2025-07-01 10:16:52 +0800

    Update TestServer.cs
    添加注释

--- Samples~/Server/TestServer.cs
+++ Samples~/Server/TestServer.cs
@@ -13,12 +13,18 @@ public class TestServer : MonoBehaviour
 
     void Start()
     {
-        ServerManager client = ServerManager.Instance;
+        //获取实例
+        ClientManager client = ClientManager.Instance;
+        //初始化客户端，以TCP
         client.InitService(NetworkProtocol.TCP);
+        //设置消息packer(json,protobuf)
         client.MessagePacker = new ProtobufPacker();
+        //设置消息分发（可选）
         client.MessageDispatcher = new OuterMessageDispatcher();
 
-        client.Connect(ip, port);
+        //连接服务器
+        client.Connect(ip,port);
+        //设置网络事件回调
         client.OnConnect += OnConnect;
         client.OnError += OnError;
         client.OnMessage += OnMessage;
