143b84091528a05475f7ff349989c3af3c8ca69d
yupu <yupu@gmail.com>
2025-09-09 18:10:09 +0800

    格式化

--- Runtime/Network/Core/ModbusTCP/ModbusTCPService.cs
+++ Runtime/Network/Core/ModbusTCP/ModbusTCPService.cs
@@ -6,56 +6,54 @@ using UnityEngine;
 
 namespace UNetwork
 {
-	/// <summary>
-	/// ModbusService的一个封装管理
-	/// </summary>
-	public sealed class ModbusTCPService : AService
-	{
-		private ModbusTCPChannel channel;
+    /// <summary>
+    /// ModbusService的一个封装管理
+    /// </summary>
+    public sealed class ModbusTCPService : AService
+    {
+        private ModbusTCPChannel channel;
 
-		public RecyclableMemoryStreamManager MemoryStreamManager = new RecyclableMemoryStreamManager();
-		
-		public ModbusTCPService()
-		{
-		}
-		
-		public override void Dispose()
-		{
-			this.channel.Dispose();
-		}
-		
-		public override AChannel GetChannel()
-		{
-			return channel;
-		}
+        public RecyclableMemoryStreamManager MemoryStreamManager = new RecyclableMemoryStreamManager();
 
-		public override AChannel ConnectChannel(IPEndPoint ipEndPoint)
-		{
-			channel = new ModbusTCPChannel(ipEndPoint, this);
-			return channel;
-		}
+        public ModbusTCPService()
+        {
+        }
 
-		public override AChannel ConnectChannel(string address)
-		{
-			IPEndPoint ipEndPoint = NetworkHelper.ToIPEndPoint(address);
-			return this.ConnectChannel(ipEndPoint);
-		}
+        public override void Dispose()
+        {
+            this.channel.Dispose();
+        }
 
-		public override void Update()
-		{
-			if (channel.IsSending)
-			{
-				return;
-			}
-			
-			try
-			{
-				channel.StartSend();
-			}
-			catch (Exception e)
-			{
-				Debug.LogError(e);
-			}
-		}
-	}
+        public override AChannel GetChannel()
+        {
+            return channel;
+        }
+
+        public override AChannel ConnectChannel(IPEndPoint ipEndPoint)
+        {
+            channel = new ModbusTCPChannel(ipEndPoint, this);
+            return channel;
+        }
+
+        public override AChannel ConnectChannel(string address)
+        {
+            IPEndPoint ipEndPoint = NetworkHelper.ToIPEndPoint(address);
+            return this.ConnectChannel(ipEndPoint);
+        }
+
+        public override void Update()
+        {
+            if (channel == null) return;
+            if (channel.IsSending) return;
+            
+            try
+            {
+                channel.StartSend();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+        }
+    }
 }
