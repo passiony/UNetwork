a78dc106a4da39b739bc841cff906dffe104553d
yupu <yupu@gmail.com>
2025-12-11 08:50:16 +0800

    扩展NoRecvClient

---
+++ Runtime/Network/ClientComponentNoRecv.cs
@@ -0,0 +1,14 @@
+﻿namespace UNetwork
+{
+    /// <summary>
+    /// 客户端的 业务逻辑 管理类
+    /// </summary>
+    public class ClientComponentNoRecv : ClientComponent
+    {
+        public override void Connect()
+        {
+            base.Connect();
+            ((TChannel)Service.GetChannel()).NoRecv = true;
+        }
+    }
+}
